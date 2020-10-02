8be9c64b19691bc727ccd1c4d33685bafc7df125
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-02 21:39:38 +0900

    pre-mypage-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'items/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :mypages, only: [:index, :show] 
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
-  root 'items#index'
+  root 'mypages#index'
+
 end
