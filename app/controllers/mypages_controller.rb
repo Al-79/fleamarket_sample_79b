5bbcc45c89ad2fa43c27f381947aeac86762c053
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-02 22:22:09 +0900

    delete-file

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,6 +2,5 @@ class MypagesController < ApplicationController
   def index
   end
 
-  def show
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'items/index'
-  resources :mypages, only: [:index, :show] 
+  # resources :mypages, only: [:index, :show] 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
   root 'mypages#index'
