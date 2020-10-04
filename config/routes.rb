c39463373268b472416de99903ca776928916893
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-04 10:41:22 +0900

    fix-routes&README

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'mypages/index'
   devise_for :users
   get 'items/index'
   # resources :mypages, only: [:index, :show] 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
-  root 'mypages#index'
+  root 'front#index'
 
 end
