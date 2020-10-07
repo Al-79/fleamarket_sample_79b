0c26200499552c59f0a21fd095c860dad5cd4ee6
Al-79 <AlorhZ@gmail.com>
2020-10-07 20:55:27 +0900

    routing

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    binding.pry
+    Item.create(item_params)
+  end
+
+  def show
+    @items = Item.all
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
   root 'front#index'
