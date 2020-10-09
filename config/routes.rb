134f9de4ac4c51730870ec9851ab8a5af1472ebb
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-10 00:52:06 +0900

    add-logouto&credit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'card/new'
+  get 'card/show'
   get 'mypages/index'
   devise_for :users
   get 'items/index'
+  resources :users, only: [:edit]
+  resources :card, only: [:new, :show]
+
   # resources :mypages, only: [:index, :show] 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
