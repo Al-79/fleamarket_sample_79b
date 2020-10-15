7f9714a04505b78718032b5942f18121bbbbd0b7
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-15 17:28:30 +0900

    fix_mypage_markup

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   get 'mypages/index'
   devise_for :users
   get 'items/index'
-  resources :users, only: [:edit]
+  resources :users, only: [:edit, :update]
   resources :card, only: [:new, :show]
 
-  # resources :mypages, only: [:index, :show] 
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # after
   root 'mypages#index'
 
 end
