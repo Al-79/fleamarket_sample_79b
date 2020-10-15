a0b7c0e0629b69aff3fd8ce76b5a9ca0e3649436
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-15 22:49:22 +0900

    re_conflict_reset

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'items/index'
   resources :users, only: [:edit, :update]
   resources :card, only: [:new, :show]
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # after
   root 'front#index'
-
 end
