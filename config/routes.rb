65154b847345e215f44c3562d4269e89443d7c6c
tomoaki-abe <zhizhangabu32@gmail.com>
2020-10-16 23:01:59 +0900

    fix-routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'mypages/index'
+  resources :mypages, only: [:index]
   devise_for :users
-  get 'items/index'
+  resources :items, only: [:index]
   resources :users, only: [:edit, :update]
   resources :card, only: [:new, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
