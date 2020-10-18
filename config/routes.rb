5dc154e6ca7593b077568180d5e5afb2000bd82c
nakanenakane <nandakakanashe@gmail.com>
2020-10-18 10:16:27 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get  'new_addresses', to: 'users/registrations#new_address'
+  end
+
   resource :items , only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # after
