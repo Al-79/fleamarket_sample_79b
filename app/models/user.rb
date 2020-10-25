891ffb2124e01cf541cf50df6dfa79e39d78644a
nakanenakane <nandakakanashe@gmail.com>
2020-10-26 00:48:10 +0900

    バリデーション更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   has_many :items
   has_many :addresses
   has_one :credit_card
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
-  VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/
-  
-  validates :nickname, presence: true, length: { maximum: 20 }
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME}
-  validates :family_name, presence: true, format: { with: VALID_NAME}
-  validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA}
-  validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA}
-  validates :birth_date, presence: true
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
+VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/
+
+validates :nickname, presence: true, length: { maximum: 20 }
+validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+validates :first_name, presence: true, format: { with: VALID_NAME}
+validates :family_name, presence: true, format: { with: VALID_NAME}
+validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA}
+validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA}
+validates :birth_date, presence: true
 end
