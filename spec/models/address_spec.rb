c01cd8142a9c8082d193b60ed4b7c824d97affb9
nakanenakane <nandakakanashe@gmail.com>
2020-10-28 01:01:56 +0900

    テスト追加

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -50,5 +50,33 @@ describe Address do
       user.valid?
       expect(user.errors[:address_line]).to include("を入力してください")
     end
+
+        # 名前全角入力のテスト ▼
+
+        it 'address_family_nameが全角入力でなければ登録できないこと' do
+          user = build(:address, address_family_name: "ｱｲｳｴｵ") # 意図的に半角入力を行いエラーを発生させる
+          user.valid?
+          expect(user.errors[:address_family_name]).to include("は無効です。再入力してください")
+        end
+    
+        it 'first_nameが全角入力でなければ登録できないこと' do
+          user = build(:address, address_first_name: "ｱｲｳｴｵ") # 意図的に半角入力を行いエラーを発生させる
+          user.valid?
+          expect(user.errors[:address_first_name]).to include("は無効です。再入力してください")
+        end
+    
+        # カタカナ全角入力のテスト ▼
+    
+        it 'address_family_name_kanaが全角カタカナでなければ登録できないこと' do
+          user = build(:address, address_family_name_kana: "あいうえお") # 意図的にひらがな入力を行いエラーを発生させる
+          user.valid?
+          expect(user.errors[:address_family_name_kana]).to include("は無効です。再入力してください")
+        end
+    
+        it 'address_first_name_kanaが全角カタカナでなければ登録できないこと' do
+          user = build(:address, address_first_name_kana: "あいうえお") # 意図的にひらがな入力を行いエラーを発生させる
+          user.valid?
+          expect(user.errors[:address_first_name_kana]).to include("は無効です。再入力してください")
+        end
   end
 end
