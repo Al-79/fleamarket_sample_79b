bfc9a3658947dbb9003c36f60ef776e4953017f6
Al-79 <AlorhZ@gmail.com>
2020-10-30 18:44:39 +0900

    model-modify
    モデルの内容を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :preparation_day
   belongs_to_active_hash :postage_payer
   belongs_to_active_hash :prefecture
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :preparation_day_id, presence: true
+  validates :postage_payer_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :price, presence: true
 end
