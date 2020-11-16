6e10cfa5c785bf2aaf2c8621a11da08b45b72da0
Al-79 <AlorhZ@gmail.com>
2020-11-16 23:29:08 +0900

    db_change?

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_014644) do
+ActiveRecord::Schema.define(version: 2020_09_25_150608) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "address_first_name", null: false
+    t.string "address_family_name", null: false
+    t.string "address_first_name_kana", null: false
+    t.string "address_family_name_kana", null: false
     t.string "post_code", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
@@ -22,10 +26,6 @@ ActiveRecord::Schema.define(version: 2020_10_25_014644) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "address_first_name", null: false
-    t.string "address_family_name", null: false
-    t.string "address_first_name_kana", null: false
-    t.string "address_family_name_kana", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_10_25_014644) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.string "ancestry", null: false
+    t.string "ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -77,6 +77,25 @@ ActiveRecord::Schema.define(version: 2020_10_25_014644) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth_date", null: false
+    t.text "introduction"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_copy", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "first_name", null: false
     t.string "family_name", null: false
