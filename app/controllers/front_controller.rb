0bea9e53bffdd4c4de1d08e2439ed278bbaa8d72
tomoaki-abe <zhizhangabu32@gmail.com>
2020-11-16 22:39:38 +0900

    画像、商品名、値段の表示

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,7 @@
 class FrontController < ApplicationController
   def index
     # @parents = Category.where(ancestry: nul)
-    @items = Item.all
+    @items = Item.where(buyer_id: nil)
     @image = ItemImage.all
   end
 
