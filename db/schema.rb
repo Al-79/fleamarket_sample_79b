778803f43c31a0545d613ca479879cc65726a907
tomoaki-abe <zhizhangabu32@gmail.com>
2020-11-21 23:31:00 +0900

    mount_uploader エラー解決

--- db/schema.rb
+++ db/schema.rb
@@ -96,25 +96,6 @@ ActiveRecord::Schema.define(version: 2020_09_25_150608) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_copy", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.date "birth_date", null: false
-    t.text "introduction"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
   add_foreign_key "addresses", "users"
   add_foreign_key "credit_cards", "users"
   add_foreign_key "item_images", "items"
