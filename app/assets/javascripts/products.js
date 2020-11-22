b9613af8b568fb84bfbeffdafd02baf557df4a7c
Al-79 <AlorhZ@gmail.com>
2020-11-22 17:01:34 +0900

    multi-image
    複数投稿ができるようになりました。
    ビューも崩れないです。
    ただし、まだ画像の確認ができません

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -5,24 +5,35 @@ $(function(){
     const html = `<div data-index="${index}" class="js-file_group">
                     <input class="js-file" type="file"
                     name="item[item_images_attributes][${index}][image]"
-                    id="item_item_images_attributes_${index}_image"><br>
-                    <div class="js-remove">削除</div>
+                    id="item_item_images_attributes_${index}_image"
+                    style="display:none"
+                    ><br>
+                    <div class="js-remove", style="display:none">削除</div>
                   </div>`;
     return html;
   }
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-  $('#item_item_images_attributes_0_image').on('change', function(e) {
-    // console.log(e.target.files[0])
+    console.log($('.input-area'))
+    $(document).on('change', ".js-file",function(e) {
     // fileIndexの先頭の数字を使ってinputを作る
-    $('.listing-image').append(buildFileField(fileIndex[0]));
+    console.log(fileIndex[0])
+    // $(`#item_images_attributes_${fileIndex[0]}_image`[0]).trigger("click")
+    $('.listing-image').append(buildFileField(fileIndex[0]))
+    console.log(111)
+    // $('.listing-camera').attr('for', `item_item_images_attributes_${fileIndex[0]}_image`)
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-
+    console.log(fileIndex[0])
+    console.log(fileIndex)
   });
+
+  $(document).on('click', '.input-area', function() {
+    $('.js-file:last').trigger('click')
+  })
+
 
   $('.listing-image').on('click', '.js-remove', function() {
     $(this).parent().remove();
